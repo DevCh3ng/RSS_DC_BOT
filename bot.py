9aeb34c235a235bf7db09d8699951ded2f2f084b
Tch3ng <devinjotedy89@gmail.com>
2025-09-11 21:42:02 +0800

    modified bot.py adjusting to /cogs/rss.py

--- bot.py
+++ bot.py
@@ -2,142 +2,93 @@ import discord
 from discord.ext import tasks,commands
 import os 
 from dotenv import load_dotenv
-import feedparser
+import asyncio
 import json
-import time
 import aiohttp
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-HISTORY = "history.json"
-ALERTS = "alerts.json"
-CONFIG = "configs.json"
 CID = os.getenv('CHANNEL_ID')
-CHANNEL_ID = int(CID)
-posted_articles = {}
-active_alerts = []
-bot_config = {}
-MIN_RSS_INTERVAL = 5
-DEFAULT_RSS_INTERVAL = 10
-
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix="-", intents=intents) # new bot instance
 
-def load_history():
+bot.HISTORY = "history.json"
+bot.ALERTS = "alerts.json"
+bot.CONFIG = "configs.json"
+
+bot.posted_articles = {}
+bot.active_alerts = []
+bot.bot_config = {}
+bot.MIN_RSS_INTERVAL = 5
+bot.DEFAULT_RSS_INTERVAL = 10
+
+bot.CHANNEL_ID = int(CID)
+
+def load_history(bot):
     """Loads the history of posted article links from file."""
-    global posted_articles
     try: 
-        with open(HISTORY, 'r') as f:    
-            posted_articles = json.load(f)
-        print(f"Loaded {len(posted_articles)} articles from history.")
+        with open(bot.HISTORY, 'r') as f:    
+            bot.posted_articles = json.load(f)
+        print(f"Loaded {len(bot.posted_articles)} articles from history.")
     except(FileNotFoundError, json.JSONDecodeError):
         print("History file not found. Creating")
-        posted_articles = {}
+        bot.posted_articles = {}
 
-def load_alerts():
+def load_alerts(bot):
     """Load alert.json"""
-    global active_alerts
     try:
-        with open(ALERTS, 'r') as f:
-            active_alerts = json.load(f)
-        print(f"Loaded {len(active_alerts)} alerts from history.")
+        with open(bot.ALERTS, 'r') as f:
+            bot.active_alerts = json.load(f)
+        print(f"Loaded {len(bot.active_alerts)} alerts from history.")
             
     except (FileNotFoundError, json.JSONDecodeError):
             print("Alert file not found. Creating")
-            active_alerts = []
+            bot.active_alerts = []
 
-def load_configs():
+def load_configs(bot):
     """Load bot configs to configs.json"""
-    global bot_config 
     try:
-        with open(CONFIG, 'r') as f:
-            bot_config = json.load(f)
-        print(f"Loaded {len(bot_config)} configs from history.")
+        with open(bot.CONFIG, 'r') as f:
+            bot.bot_config = json.load(f)
+        print(f"Loaded {len(bot.bot_config)} configs from history.")
     except (FileNotFoundError, json.JSONDecodeError):
         print("Config file not found. Creating")
-        bot_config = {}
+        bot.bot_config = {}
 
-def save_history():
-    with open(HISTORY, 'w') as f:
-        json.dump(posted_articles, f, indent = 4)
+def save_history(bot):
+    with open(bot.HISTORY, 'w') as f:
+        json.dump(bot.posted_articles, f, indent = 4)
 
-def save_alerts():
-    with open(ALERTS, 'w') as f:
-        json.dump(active_alerts, f, indent=4)
+def save_alerts(bot):
+    with open(bot.ALERTS, 'w') as f:
+        json.dump(bot.active_alerts, f, indent=4)
 
-def save_configs():
-    with open(CONFIG, 'w') as f:
-        json.dump(bot_config, f, indent=4)
-async def perform_rss_check():
-    """Main logic for fetching, parsing, and checking RSS feed"""
-    global posted_articles
-    await bot.wait_until_ready()
-    crypto_url = "https://cointelegraph.com/rss"
-    feed = await bot.loop.run_in_executor(None, lambda: feedparser.parse(crypto_url))
-    if feed.entries:
-        latest = feed.entries[0]
+def save_configs(bot):
+    with open(bot.CONFIG, 'w') as f:
+        json.dump(bot.bot_config, f, indent=4)
 
-        # check if article link is not on HISTORY
-        if latest.link not in posted_articles:
-            print(f"New article found: {latest.title}")
-
-            posted_articles[latest.link] = time.time()
-            
-            channel = bot.get_channel(CHANNEL_ID)
-            if channel:
-                embed = discord.Embed(
-                    title = latest.title,
-                    url = latest.link,
-                    description="A new article has been posted",
-                    color = discord.Color.blue()
-                )
-                embed.set_footer(text=feed.feed.title)
-                if hasattr(latest, 'media_content') and latest.media_content:
-                    image_url = latest.media_content[0]['url']
-                    embed.set_image(url=image_url)
-                await channel.send(embed=embed)
-            else:
-                print(f"Error: Channel with ID {CHANNEL_ID} not found.")
-        
-        curr_time = time.time()
-        three_hours = 3*60*60
-
-        prune = {
-            link: timestamp
-            for link, timestamp in posted_articles.items()
-            if (curr_time - timestamp) < three_hours
-        }
-        if len(prune) < len(posted_articles):
-            print(f"Pruned {len(posted_articles) - len(prune)} articles from history.")
-            posted_articles = prune 
-        save_history()
-
-    else:
-        print(f"Could not find any articles in the feed: {crypto_url}")
-
+bot.load_configs = lambda: load_configs(bot)
+bot.save_configs = lambda: save_configs(bot)
+bot.load_alerts = lambda: load_alerts(bot)
+bot.save_alerts = lambda: save_alerts(bot)
+bot.load_history = lambda: load_history(bot)
+bot.save_history = lambda: save_history(bot)
 
 # event: on_ready
 # runs once it got connected to discord
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord')
-    load_history()
-    load_alerts()
-    load_configs()
+    bot.load_history()
+    bot.load_alerts()
+    bot.load_configs()
 
-    interval = bot_config.get('rss_interval_minutes', DEFAULT_RSS_INTERVAL)
-    fetch_rss.change_interval(minutes = interval)
-
-    print("Init RSS checking")
-    await perform_rss_check()
-
-    if not fetch_rss.is_running():
-        fetch_rss.start()
-    if not check_prices.is_running():
-        check_prices.start()
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
 # Prefix command for :ping
 @bot.command(name="ping")
@@ -147,32 +98,6 @@ async def prefix_ping(prefix):
 @bot.group(invoke_without_command=True)
 async def alert(prefix):
     await prefix.send("Alert command. Use `:alert add <crypto> <condition> <price>`")
-
-@bot.group(invoke_without_command=True)
-async def rss(prefix):
-    curr_interval = fetch_rss.minutes
-    await prefix.send(f"RSS Settings. The curent check interval is **{curr_interval}** minutes. Use -rss interval <minutes> to change")
-    
-@rss.command(name="interval")
-@commands.has_permissions(administrator=True)
-async def set_rss_interval(prefix, new_interval: int):
-    if new_interval < MIN_RSS_INTERVAL:
-        await prefix.send(f"❌ Minimum RSS poll interval rate is **5 Minutes**.")
-        return
-    fetch_rss.change_interval(minutes=new_interval)
-    bot_config['rss_interval_minutes'] = new_interval
-    save_configs()
-    await prefix.send(f"✅ RSS poll interval is now ** {new_interval} minutes**.")
-
-@set_rss_interval.error
-async def interval_error(prefix,error):
-    if isinstance(error, commands.MissingPermissions):
-        await prefix.send(f"❌ **Error:** You don't have any previledge to use this command")
-    elif isinstance(error, commands.BadArgument):
-        await prefix.send(f"❌ **Error:** Please provide a valid number")
-    else:
-        print(f"RSS command error: {error}")
-        await prefix.send("An unexpected error occured")
 
 @alert.command (name="add")
 async def add_alert(prefix, crypto: str, condition: str, price: float):
@@ -287,10 +212,15 @@ async def check_prices():
             else:
                 print(f"Error fetching prices, status: {response.status}")
 
-@tasks.loop(minutes=10)
-async def fetch_rss():
-    await perform_rss_check()
-if TOKEN is None:
-    print("TOKEN ERROR")
-else:
-    bot.run(TOKEN)
+async def main():
+    async with bot:
+        await load_cogs()
+        await bot.start(TOKEN)
+
+if __name__ == "__main__":
+    if TOKEN == None:
+        print("Token ERROR")
+    elif bot.CHANNEL_ID == None:
+         print("Channel id error")
+    else:
+        asyncio.run(main())
